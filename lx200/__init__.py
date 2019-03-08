d2ad60c4237ec360b40fee908d177fffeb0df13d
Adrián Pardini <github@tangopardo.com.ar>
2019-03-08 19:31:16 -0300

    adds parse example

--- lx200/__init__.py
+++ lx200/__init__.py
@@ -1,3 +1,3 @@
 __import__('pkg_resources').declare_namespace(__name__)
 __version__ = '0.0.2'
-__all__ = ['commands', 'responses', 'parser']
+__all__ = ['commands', 'responses', 'parser', 'examples']
---
+++ lx200/examples/parse.py
@@ -0,0 +1,13 @@
+import fileinput
+
+from lx200.parser import Parser
+
+
+if __name__ == '__main__':
+
+    parser = Parser()
+    for line in fileinput.input():
+        parser.feed(line)
+        while(parser.output):
+            command = parser.output.pop()
+            print(command)
