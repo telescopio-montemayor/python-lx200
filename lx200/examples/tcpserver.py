54c41266f7e75b8cedcac1b75adaf960d6398053
Adrián Pardini <github@tangopardo.com.ar>
2019-03-12 17:35:43 -0300

    examples/tcpserver: adds (verbose) logging

--- lx200/examples/tcpserver.py
+++ lx200/examples/tcpserver.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 import asyncio
+import logging
 
 from lx200.parser import Parser
 from lx200.store import Store
 import lx200.responses
+
+
+logger = logging.getLogger('lx200.examples.tcpserver')
 
 
 class LX200Protocol(asyncio.Protocol):
@@ -15,10 +19,15 @@ class LX200Protocol(asyncio.Protocol):
 
     def connection_made(self, transport):
         self.transport = transport
-        print('Connected: ', transport.get_extra_info('socket', 'peername'))
+        transport.set_write_buffer_limits(high=0, low=0)
+        logger.info('Connected: {}'.format(transport.get_extra_info('socket', 'peername')))
 
     def data_received(self, data):
-        self.parser.feed(data.decode('ascii'))
+        decoded_data = data.decode('ascii')
+        self.parser.feed(decoded_data)
+
+        logger.debug('<< {}'.format(decoded_data))
+
         while self.parser.output:
             command = self.parser.output.pop()
             self.store.commit_command(command)
@@ -26,17 +35,28 @@ class LX200Protocol(asyncio.Protocol):
             response = lx200.responses.for_command(command)
 
             self.store.fill_response(response)
+
+            logger.debug('>> {}'.format(repr(response)))
+            logger.debug('>> {}'.format(str(response)))
+
             self.transport.write(bytes(str(response), 'ascii'))
 
 
 if __name__ == '__main__':
     import argparse
 
+    logging.basicConfig()
+    logger.setLevel(logging.INFO)
+
     parser = argparse.ArgumentParser(description='Simulates an LX200 compatible telescope with tcp connection', prog='lx200.examples.tcpserver')
 
+    parser.add_argument('--verbose', required=False, default=False, action='store_true')
     parser.add_argument('--port', type=int, required=False, default=7634)
     parser.add_argument('--host', type=str, required=False, default='127.0.0.1')
     args = parser.parse_args()
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
 
     store = Store()
 
@@ -47,8 +67,8 @@ if __name__ == '__main__':
     coro = loop.create_server(protocol_factory, args.host, args.port)
     server = loop.run_until_complete(coro)
 
-    print('LX200 TCP server example')
-    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    logger.info('LX200 TCP server example')
+    logger.info('Serving on {}'.format(server.sockets[0].getsockname()))
 
     loop.run_forever()
 
