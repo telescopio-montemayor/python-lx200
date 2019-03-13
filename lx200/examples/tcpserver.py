f5e7bfb4ae0fda118f7a0af84c153d61827e968e
Adrián Pardini <github@tangopardo.com.ar>
2019-03-13 08:03:07 -0300

    examples/tcpserver: also serve store as json
    We start an http server (by default on 8081) that exposes the current
    store of scope status.

--- lx200/examples/tcpserver.py
+++ lx200/examples/tcpserver.py
@@ -3,6 +3,8 @@
 import asyncio
 import logging
 
+from aiohttp import web
+
 from lx200.parser import Parser
 from lx200.store import Store
 import lx200.responses
@@ -42,7 +44,7 @@ class LX200Protocol(asyncio.Protocol):
             self.transport.write(bytes(str(response), 'ascii'))
 
 
-if __name__ == '__main__':
+async def run():
     import argparse
 
     logging.basicConfig()
@@ -52,6 +54,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--verbose', required=False, default=False, action='store_true')
     parser.add_argument('--port', type=int, required=False, default=7634)
+    parser.add_argument('--web-port', type=int, required=False, default=8081)
     parser.add_argument('--host', type=str, required=False, default='127.0.0.1')
     args = parser.parse_args()
 
@@ -63,15 +66,31 @@ if __name__ == '__main__':
     def protocol_factory(*args, **kwargs):
         return LX200Protocol(store, *args, **kwargs)
 
-    loop = asyncio.get_event_loop()
-    coro = loop.create_server(protocol_factory, args.host, args.port)
-    server = loop.run_until_complete(coro)
+    loop = asyncio.get_running_loop()
+    server = await loop.create_server(protocol_factory, args.host, args.port)
+
+    routes = web.RouteTableDef()
+
+    @routes.get('/')
+    async def json_store_status(request):
+        return web.json_response(request.app['scope_store'])
+
+    app = web.Application()
+    app['scope_store'] = store
+
+    app.add_routes(routes)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, args.host, args.web_port)
+    await site.start()
 
     logger.info('LX200 TCP server example')
     logger.info('Serving on {}'.format(server.sockets[0].getsockname()))
+    logger.info('Serving state on http://{}:{}'.format(args.host, args.web_port))
 
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
     loop.run_forever()
-
-    server.close()
-    loop.run_until_complete(server.wait_closed())
-    loop.close()
